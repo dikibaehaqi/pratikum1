080caa7814d9ae27b55e65afbddf019ac1995a4d
baehaqi <baehakisaputra72@gmail.com>
2021-10-28 00:25:40 +0700

    menambahkan Tugaslatihan.py

---
+++ Tugaslatihan.py
@@ -0,0 +1,30 @@
+print("Masukan Nama Lengkap :")
+Nama_Lengkap = str(input())
+
+print("Masukan Nama Panggilan :")
+Nama_Panggilan = str(input())
+
+print("Masukan NPM :")
+Isikan_NPM = str(input())
+
+print("Masukan Tempat Lahir :")
+Tempat_Lahir = str(input())
+
+print("Masukan Tanggal Lahir :")
+Tanggal_Lahir = str(input())
+
+print("Masukan No Telepon :")
+No_Telepon = str(input())
+
+print("Masukan Alamat :")
+Alamat = str(input())
+
+print("Usia")
+Usia = str(input())
+
+print("\nAssalamu'alaikum \n"),
+print("Let me introduce my self.","My name is", Nama_Lengkap,", but you can call me", Nama_Panggilan,
+". My NPM is", Isikan_NPM,". I was born in", Tempat_Lahir, "and i am",Usia,
+"years old.", "i am very glad if you want to invite my house in",Alamat,
+". So, dont forget to call me before whit the number", No_Telepon,"\n")
+print("Thank You")
